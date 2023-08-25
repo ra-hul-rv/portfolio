94ede19c31d45d9d46e813551c4a4779c9228fe0
rahul.rv <rahul.rv@inapp.com>
2023-08-25 15:41:37 +0530

    starting

---
+++ src/Components/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <header className='z-[999] relative' >
+        <div className=' bg-white  w-full fixed
+         top-0 left-1/2 h-[4.5rem] rounded-none border
+          border-white border-opacity-80 shadow-lg ' >
+
+        </div>
+    </header>
+  )
+}
+
+export default header
